ae826eaf994a52d9de47bb2aa3500e3f0c056ea2
Rogier De Ruijter <rogierderuijter@hotmail.com>
2020-11-04 23:26:38 +0100

    split off offset

--- core.js
+++ core.js
@@ -12,16 +12,6 @@ const finish = document.getElementById("finish");
 
 const widthFish = fish1.offsetWidth;
 
-function getOffset( el ) {
-    var _x = 0;
-    var _y = 0;
-    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-        _x += el.offsetLeft - el.scrollLeft;
-        _y += el.offsetTop - el.scrollTop;
-        el = el.offsetParent;
-    }
-    return { top: _y, left: _x };
-}
 var finishLine = 0;
 
 const calculateFinishLinePosition = () => {
